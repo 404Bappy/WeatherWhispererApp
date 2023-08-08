853b4aefbad6b69d74be1808a7da7751f4cd6d14
Bappy71 <19103076@iubat.edu>
2023-08-08 11:43:04 +0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {StyleSheet, View} from 'react-native';
+import Navigation from './SRC/Components/Navigation/Navigation';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Navigation />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8688',
+  },
+});
